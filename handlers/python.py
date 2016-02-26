5674426c8c8a1a423269fb3e18aa67ef582fa008
rubenhortas <rubenhortas@gmail.com>
2016-02-26 18:18:45 +0100

    Added

---
+++ handlers/python.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*
+
+"""
+@author:  Rubén Hortas Astariz <http://rubenhortas.blogspot.com>
+@contact: rubenhortas at gmail.com
+@github:  http://github.com/rubenhortas
+@license: CC BY-NC-SA 3.0 <http://creativecommons.org/licenses/by-nc-sa/3.0/>
+@file:    python  
+"""
+
+
+# noinspection PyUnusedLocal
+def exit_signal_handler(signal, frame):
+    """"
+    exit_signal_handler(signal, frame)
+        Handles an exit signal.
+    Arguments:
+        signal: (int) number of signal.
+        frame: (string) name of the signal handler.
+    """
+
+    exit(0)
