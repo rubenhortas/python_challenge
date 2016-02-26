dfb9a8baee671c98f98948796f5a7f5f4b408277
rubenhortas <rubenhortas@gmail.com>
2016-02-26 19:41:06 +0100

    Added

--- 05.py
+++ 05.py
@@ -10,3 +10,29 @@
 """
 
 # http://www.pythonchallenge.com/pc/def/peak.html
+
+import urllib2
+import pickle
+import signal
+from handlers.python import exit_signal_handler
+
+if __name__ == '__main__':
+
+    signal.signal(signal.SIGINT, exit_signal_handler)
+
+    try:
+        str = ''
+
+        response = urllib2.urlopen("http://www.pythonchallenge.com/pc/def/banner.p")
+        data = pickle.load(response)
+        response.close()
+
+        for l in data:  # data composed of lists
+            for t in l:  # lists composed by tuples
+                str = '{0}{1}'.format(str, (t[1] * t[0]))
+            str = '{0}\n'.format(str)
+
+        print str
+
+    except Exception as e:
+        print(e.message)
