e3682fda321efe23c5d5374f20667f1622fa77ce
rubenhortas <rubenhortas@gmail.com>
2016-02-26 18:10:02 +0100

    Enhanced

--- 01.py
+++ 01.py
@@ -14,6 +14,7 @@
 import string
 
 if __name__ == '__main__':
+    # noinspection PyPep8
     text = 'g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr\'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj. '
     trans = string.maketrans('abcdefghijklmnopqrstuvwxyz', 'cdefghijklmnopqrstuvwxyzab')
     print text.translate(trans)
