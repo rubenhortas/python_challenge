2386cdb3af2502b9b4166ad757641cd35433128c
rubenhortas <rubenhortas@gmail.com>
2022-05-19 20:10:57 +0200

    Renamed variable

--- 02.py
+++ 02.py
@@ -2,14 +2,14 @@
 import re
 
 if __name__ == '__main__':
-    rare_characters = re.compile('[a-zA-Z\\d ]', re.UNICODE)
+    re = re.compile('[a-zA-Z\\d ]', re.UNICODE)
     result = ""
 
     try:
         f = open('02_text.txt', 'r')
 
         for line in f.readlines():
-            match = rare_characters.search(line)
+            match = re.search(line)
 
             if match:
                 result = "%s%s" % (result, match.group(0))
